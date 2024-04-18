16db060c5fc6729eb37c2fb6c5a9a4b39d54c70a
Laurynassa <144778238+Laurynassa@users.noreply.github.com>
2024-04-18 10:42:03 +0200

    Update CountriesService.jsx

--- src/Services/CountriesService.jsx
+++ src/Services/CountriesService.jsx
@@ -4,7 +4,7 @@ const API_URL = "https://restcountries.com/v3.1/all";
 
 const getAllCountriesInfo = async () => {
   try {
-    const res = axios.get(API_URL);
+    const res = await axios.get(API_URL);
     if (res.data !== undefined) {
       return res.data;
     }
